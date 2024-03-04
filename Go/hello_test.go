e6a5b54c3ceb73f3969b83397f6ba739a7e65041
Pedro Correa <pedro8correa@gmail.com>
2024-03-04 15:26:11 -0300

    :sparkles: (Go) test with default value

--- Go/hello_test.go
+++ Go/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello("Pedro")
-    want := "Hello, Pedro"
+    t.Run("saying hello to people", func(t *testing.T) {
+	got := Hello("Pedro")
+	want := "Hello, Pedro"
 
-    if got != want {
-       t.Errorf("got %q want %q", got, want)
-    }
+	if got != want {
+	    t.Errorf("got %q want %q", got, want)
+	}
+    })
+
+    t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+	got := Hello("")
+	want := "Hello, world"
+
+	if got != want {
+	    t.Errorf("got %q want %q", got, want)
+	}
+    })
 }
+
