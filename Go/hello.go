504815c05de776db590278ee113114411f8c2046
Pedro Correa <pedro8correa@gmail.com>
2024-03-04 16:29:35 -0300

    :sparkles: (Go) hello in french

--- Go/hello.go
+++ Go/hello.go
@@ -3,19 +3,26 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
-     if name == "" {
-     	name = "world"
-     }
+    if name == "" {
+        name = "world"
+    }
 
-     if language == spanish {
-        return spanishHelloPrefix + name
-     }
+    prefix := englishHelloPrefix
 
-     return englishHelloPrefix + name
+    switch language {
+        case french:
+            prefix = frenchHelloPrefix
+        case spanish:
+            prefix = spanishHelloPrefix
+    }
+
+    return prefix + name
 }
 
 func main() {
