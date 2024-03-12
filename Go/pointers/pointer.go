cb38f0871a4f35a104c694cd602be264fb3e339c
Pedro Correa <pedro8correa@gmail.com>
2024-03-11 22:03:32 -0300

    :sparkles: (Go) pointers

---
+++ Go/pointers/pointer.go
@@ -0,0 +1,38 @@
+package pointers
+
+import (
+    "fmt"
+    "errors"
+)
+
+type Bitcoin int
+
+type Stringer interface {
+    String() string
+}
+
+func (b Bitcoin) String() string {
+    return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+    // when using lowercase properties names, it will be private
+    balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+    w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+    return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+    if amount > w.balance {
+        return errors.New("cannot withdraw, insufficient funds")
+    }
+
+    w.balance -= amount
+    return nil
+}
