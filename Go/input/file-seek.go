78177dddc99be0f29a9b56130f0b56942296ea3a
Pedro Correa <pedro8correa@gmail.com>
2025-05-26 18:48:03 -0300

    :sparkles: (Go) file seek

---
+++ Go/input/file-seek.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	tmp := os.TempDir()
+	file, err := os.Create(tmp + "/myfile")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	msg := "Save the world with Go!"
+
+	_, err = file.WriteString(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	positions := []int{4, 10, 20}
+	for _, i := range positions {
+		_, err := file.Seek(int64(i), 0)
+		if err != nil {
+			panic(err)
+		}
+		file.Write([]byte("X"))
+	}
+
+	file.Seek(0, 0)
+
+	result := make([]byte, len(msg))
+	if _, err := file.Read(result); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", result)
+}
