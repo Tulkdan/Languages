957112487bafa65f7c2bb8c2c5f1e60a08a63f5d
Pedro Correa <pedro8correa@gmail.com>
2025-05-26 18:48:03 -0300

    :sparkles: (Go) templates

---
+++ Go/templates/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"text/template"
+)
+
+const letter = `Dear {{.Name}},
+{{if .Attended}}
+It was a pleasure to see you at the wedding.
+{{- else}}
+It is a shame you couldn't make it to the wedding.
+{{- end}}
+Best wishes,
+Alex`
+
+func main() {
+	people := []struct {
+		Name     string
+		Attended bool
+	}{
+		{
+			Name:     "Alex",
+			Attended: true,
+		},
+		{
+			Name:     "Mick",
+			Attended: false,
+		},
+	}
+
+	t := template.Must(template.New("letter").Parse(letter))
+
+	for _, person := range people {
+		buffer := bytes.Buffer{}
+		err := t.Execute(&buffer, person)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Letter for %s:\n\n%s\n\n", person.Name, buffer.String())
+	}
+}
